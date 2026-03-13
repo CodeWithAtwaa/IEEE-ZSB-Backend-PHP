06b574085d52a08c0b09097793ccb4336ef933ee
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-13 17:17:07 +0200

    indexed Array

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,3 +1,5 @@
+-- Create Our DB
 CREATE DATABASE IEEE_Backend_PHP
 
+-- USe DB 
 use IEEE_Backend_PHP;
--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IEEE Backend PHP</title>
 </head>
+
 <body>
-    <h1>Mohamed TAmer Atwaa</h1>
+
+    <?php
+    $names = [
+        "Mohamed 1 TAmer",
+        "Mohamed 2 TAmer",
+        "Mohamed 3 TAmer",
+        "Mohamed 4 TAmer",
+        "Mohamed 5 TAmer",
+    ];
+    ?>
+
+    <h1>Users names</h1>
+    <ul>
+        <?php
+        foreach ($names as $name):
+        ?>
+            <li>Mohamed 1 TAmer</li>
+        <?php
+        endforeach;
+        ?>
+    </ul>
+        <?php
+        foreach ($names as $name):
+            echo "<li>" . $name . "</li>";
+        endforeach;
+        ?>
+    </ul>
 </body>
+
 </html>
