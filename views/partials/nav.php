57f27662cced4de1764d8fae2a82941cfadc2f2d
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-15 00:33:29 +0200

    first superglobals

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,9 +8,27 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                                <a href="/" aria-current="page" class="rounded-md bg-gray-900 text-white px-3 py-2 text-sm font-medium ">Home</a>
-                                <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                                <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                                <!-- Home Links -->
+                                <a href="/" aria-current="page" class="
+                                <?php
+                                echo UrlIs('/') ? "bg-gray-900 text-white" : "text-gray-300";
+                                ?>
+                                rounded-md  px-3 py-2 text-sm font-medium  hover:bg-white/5 hover:text-white">Home</a>
+
+                                <!-- About Links -->
+                                <a href="/about.php" class="
+                                                                    <?php
+                                                                    echo UrlIs('/about.php') ? "bg-gray-900 text-white" : "text-gray-300";
+                                                                    ?>
+                                rounded-md px-3 py-2 text-sm font-medium  hover:bg-white/5 hover:text-white">About</a>
+
+
+                                <!-- Contact Links -->
+                                <a href="/contact.php" class="
+                                                                    <?php
+                                                                    echo UrlIs('/contact.php') ? "bg-gray-900 text-white" : "text-gray-300";
+                                                                    ?>
+                                rounded-md px-3 py-2 text-sm font-medium  hover:bg-white/5 hover:text-white">Contact</a>
                             </div>
                         </div>
                     </div>
