eca40f9db66607bf9bf00a4efcf5df3a720e27ac
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-15 22:21:43 +0200

    Environment and config flixabl

---
+++ Database.php
@@ -0,0 +1,25 @@
+<?php
+
+use PDO;
+
+class Database
+{
+    public $connection;
+    public function __construct($config, $username, $password)
+    {
+
+        $dsn = "mysql:" . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+    }
+
+    public function query($query)
+    {
+        $statment = $this->connection->prepare($query);
+        $statment->execute();
+
+        return $statment;
+    }
+}
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$username = 'root';
+$password =  'MyRoot@1234';
+return [
+    'host' => "localhost",
+    'port' => 3306,
+    'dbname' => "IEEE_Backend_PHP",
+    "charset" => 'utf8mb4'
+];
