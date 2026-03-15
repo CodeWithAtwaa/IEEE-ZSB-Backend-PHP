798483cc8a38e827cb0cf2381c71f14c081f88e9
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-15 22:27:49 +0200

    SQL injection

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require("functions.php");
 require("Database.php");
 $config = require ("config.php");
 
-$pdo = new Database($config, $username , $password);
+$pdo = new Database($config['database'], $config['username'] , $config['password']);
 dd($pdo->query("SELECT * FROM posts")->fetchAll());
 
 require('router.php');
