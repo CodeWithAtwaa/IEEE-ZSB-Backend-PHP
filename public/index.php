023e532a703f30a337437d64aedcef2f1f5f63f7
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-27 03:34:37 +0200

    namespacing , what and why

--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
 <?php
+
 session_start();
 
-const BASE_PATH = __DIR__ . "/../";
-require BASE_PATH . ("functions.php");
+const BASE_PATH = __DIR__ . '/../';
 
-spl_autoload_register(function($class ) {
-    require base_path("Core/{$class}.php");
+require BASE_PATH . 'Core/functions.php';
+
+spl_autoload_register(function ($class) {
+
+    $path = base_path(str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+
+    if (file_exists($path)) {
+        require $path;
+    }
+
 });
-
-// require base_path("Database.php");
-// require base_path("Response.php");
-
-
 
 require base_path('Core/router.php');
