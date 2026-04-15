ec14387d64a88e7380fe5c9eb69898630afed60f
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-04-15 21:52:26 +0200

    end Task 6

---
+++ docs/main.php
@@ -0,0 +1,9 @@
+<?php
+
+$id = 10;
+echo"Mohamed Tamer and his id is: ${id}" . PHP_EOL; 
+echo"Mohamed Tamer and his id is:" . $id . PHP_EOL;
+echo"Mohamed Tamer and his id is: {$id} " .PHP_EOL;
+
+
+
