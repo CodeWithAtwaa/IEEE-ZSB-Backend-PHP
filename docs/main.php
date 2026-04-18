9f17530d12a0fdba2a2da0d912603be26c7e46f6
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-04-18 13:51:02 +0200

    End From part one in oop usig php

--- docs/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$id = 10;
-echo"Mohamed Tamer and his id is: ${id}" . PHP_EOL; 
-echo"Mohamed Tamer and his id is:" . $id . PHP_EOL;
-echo"Mohamed Tamer and his id is: {$id} " .PHP_EOL;
-
-
-
